44f9ac816cc2ae385288109586fa8f2829f00702
devIteller <119969771+devIteller@users.noreply.github.com>
2024-02-12 01:31:52 +0330

    Add files via upload

---
+++ Pages/Index/index.jsx
@@ -0,0 +1,29 @@
+﻿import { useEffect } from "react";
+import ContentSec from "../../Components/ContentSec/ContentSec";
+import Feature from "../../Components/Feature/Feature";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import Hero from "../../Components/Hero/Hero";
+import Insights from "../../Components/Insights/Insights";
+import ReceiveInsights from "../../Components/ReceiveInsights/ReceiveInsights";
+import SliderSec from "../../Components/SliderSec/SliderSec";
+
+export default function Landing() {
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0)
+    }, 300);
+  }, []);
+  return (
+    <>
+      <Header />
+      <Hero />
+      <ContentSec />
+      <Feature />
+      <Insights />
+      <ReceiveInsights />
+      <SliderSec />
+      <Footer />
+    </>
+  );
+}
